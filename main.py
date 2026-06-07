acfa74a312ab2ebbfc52a539ce7ed7b2d1b3a4f4
araquelbarboza <araquelbarboza@gmail.com>
2026-06-07 20:45:19 -0300

    Implementação da função de listagem de documentos

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ if not os.path.exists(PASTA_DOCUMENTOS):
     os.makedirs(PASTA_DOCUMENTOS)
 
 print("Sistema de Biblioteca Digital iniciado.")
+
+import os
+
+PASTA_DOCUMENTOS = "documentos"
+
+if not os.path.exists(PASTA_DOCUMENTOS):
+    os.makedirs(PASTA_DOCUMENTOS)
+
+print("Sistema de Biblioteca Digital iniciado.")
+
+
+def listar_documentos():
+    arquivos = os.listdir(PASTA_DOCUMENTOS)
+
+    if not arquivos:
+        print("Nenhum documento encontrado.")
+    else:
+        print("\nDocumentos disponíveis:")
+        for arquivo in arquivos:
+            print("-", arquivo)
+
+listar_documentos()
